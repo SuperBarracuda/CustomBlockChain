543af2ec12318a05e128829a23a2d4dfa39a90c8
Tris <tristram.dev@gmail.com>
2021-01-24 12:34:04 +0000

    Fleshed out new transaction.

--- blockchain.py
+++ blockchain.py
@@ -12,9 +12,22 @@ class Blockchain(object):
         # Create a new Block and adds it to the chain
         pass
 
-    def new_transaction(self):
-        # Adds a new transaction to the list of transactions
-        pass
+    def new_transaction(self, sender, recipient, amount):
+        """"
+            Creates a new transaction to go into the next block
+            :param sender: <str> Address of the sender
+            :param recipient: <str> Address of the recipient
+            :param amount: <int> Amount
+            :return: <int> The index of the block that will hold this transaction
+        """
+
+        self.current_transactions.append({
+            'sender' : sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1#index of block to be added to
 
     @staticmethod
     def hash(block)
